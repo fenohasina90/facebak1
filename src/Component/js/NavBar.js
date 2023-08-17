e96756ea3bad2115b5c715b5f1bd810f7506c623
Fenohasina MANGALAHY <hei.fenohasina@gmail.com>
2023-08-17 11:03:43 +0300

    feat: update facebak structure

--- src/Component/js/NavBar.js
+++ src/Component/js/NavBar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome, faUsers, faBell, faUser } from '@fortawesome/free-solid-svg-icons';
-import './style.css';
+import '../css/style.css';
 
 function NavBar() {
   return (
