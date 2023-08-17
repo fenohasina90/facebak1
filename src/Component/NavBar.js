2a684ab5035c6c9849f1bcd3f295cfd5b5ec2226
Fitahi <hei.tsiaroniaina@gmail.com>
2023-08-17 09:28:00 +0300

    initial project

---
+++ src/Component/NavBar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faUsers, faBell, faUser } from '@fortawesome/free-solid-svg-icons';
+import './style.css';
+
+function NavBar() {
+  return (
+    <nav className="navbar">
+      <div className="logo">   
+
+        Facebak
+      </div>
+      <div className="search-bar">
+        <input type="text" placeholder="Rechercher..." />
+      </div>
+      <div className="nav-links">
+        <a href="#"><FontAwesomeIcon icon={faHome} /> Accueil</a>
+        <a href="#"><FontAwesomeIcon icon={faUsers} /> Amis</a>
+        <a href="#"><FontAwesomeIcon icon={faBell} /> Notifications</a>
+      </div>
+      <div className="profile">
+        <FontAwesomeIcon icon={faUser} />
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
