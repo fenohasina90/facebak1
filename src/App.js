4d1f11be34f14b8761e791eeb404235f10e1ad81
Fitahi <hei.tsiaroniaina@gmail.com>
2023-08-26 11:04:32 +0300

    feat : add component in App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import Main from "./Component/js/Main";
 import NavBar from "./Component/js/NavBar";
-
 
 function App() {
   return (
+    <div>
     <NavBar></NavBar>
+    <Main></Main>
+    </div>
   );
 }
 
